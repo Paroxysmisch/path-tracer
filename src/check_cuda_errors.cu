40430a30e6035bd2d0611d183a53bc051ab9d34b
Yash Shah <yashshah04.03.2002@outlook.com>
2022-10-05 20:36:53 +0100

    check_cuda_errors

---
+++ src/check_cuda_errors.cu
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "check_cuda_errors.h"
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+        file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        checkCudaErrors(cudaDeviceReset());
+        exit(99);
+    }
+}
--- src/main.cu
+++ src/main.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "check_cuda_errors.h"
 
 int main() {
     std::cout << "Hello World" << std::endl;
