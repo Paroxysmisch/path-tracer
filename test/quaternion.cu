49830e93df8b589ee692f3c57da53d6f6f00e8c9
Yash Shah <yashshah04.03.2002@outlook.com>
2022-10-13 14:29:25 +0100

    Quaternion test added and proposal timeline change

--- test/quaternion.cu
+++ test/quaternion.cu
@@ -7,16 +7,27 @@ TEST_CASE("Quaternion", "[util]") {
     pathtracer::vec3 v1{0, 1, 0};
     pathtracer::vec3 v2{1,0,0};
 
-    SECTION("2D 90deg ACW Rotation") {
+    SECTION("2D 90deg ACW rotation") {
         pathtracer::quaternion q = pathtracer::quaternion::get_rotation_between(v1, v2);
         pathtracer::vec3 expected{1,0,0};
 
-        std::cout << q.w << " "
-                  << q.ijk.x << " "
-                  << q.ijk.y << " "
-                  << q.ijk.z << " " << std::endl;
-
         bool res = (pathtracer::quaternion::rotate_vector_by_quaternion(v1, q) == expected);
+
+        REQUIRE(res == true);
+    }
+
+    SECTION("3D rotation") {
+        pathtracer::vec3 v3{-1.f, -1.f, -1.f};
+        pathtracer::vec3 v4{-1.f, -1.f, 0.f};
+        pathtracer::vec3 expected{-sqrtf(2.f / 3.f),
+                                  -sqrtf(2.f / 3.f),
+                                   sqrtf(2.f / 3.f)};
+
+        pathtracer::quaternion q = pathtracer::quaternion::get_rotation_between(v3, v4);
+
+        pathtracer::vec3 res_vec = pathtracer::quaternion::rotate_vector_by_quaternion(v4, q);
+
+        bool res = (res_vec == expected);
 
         REQUIRE(res == true);
     }
