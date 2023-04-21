ebb605ee34bba0d0f3148b8cb1e60eb41d1ca9cf
Yash Shah <yashshah04.03.2002@outlook.com>
2023-04-21 18:22:37 +0100

    Created a gold balls scene

--- src/main.cu
+++ src/main.cu
@@ -10,47 +10,98 @@
 int main() {
     std::cout << "Hello World" << std::endl;
 
-    pathtracer::camera camera(1000, 1000, pathtracer::pi / 2.f, {0.f, 0.f, -0.85f}, {0.f, 0.f, 0.f}, {0.f, 1.f, 0.f}, pathtracer::mat4::get_identity());
+    pathtracer::camera camera(2000, 2000, pathtracer::pi / 2.f, {0.f, 0.f, -0.85f}, {0.f, 0.f, 0.f}, {0.f, 1.f, 0.f}, pathtracer::mat4::get_identity());
+
+     // pathtracer::object obj0{pathtracer::SPHERE,
+     //      pathtracer::sphere(pathtracer::mat4::get_translation(-0.5f, 0.f, -0.8f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
+     //      pathtracer::LIGHT,
+     //      pathtracer::phong({0.95f, 0.25f, 0.5f}, 0.3, 0.7, 0.5, 10)};
+     // obj0.mat_d.light = pathtracer::light({0.95f, 0.4f, 0.25});
+
+     // pathtracer::object obj1{pathtracer::SPHERE,
+     //      pathtracer::sphere(pathtracer::mat4::get_translation(-0.5f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
+     //      pathtracer::MICROFACET,
+     //      pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     // obj1.mat_d.microfacet = pathtracer::microfacet{{0.35f, 0.25f, 0.85f}, {0.f, 0.f, 0.f}, 1.f, 0.18f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
+
+     // pathtracer::object obj2{pathtracer::SPHERE,
+     //      pathtracer::sphere(pathtracer::mat4::get_translation(0.5f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
+     //      pathtracer::MICROFACET,
+     //      pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     // obj2.mat_d.microfacet = pathtracer::microfacet{{0.35f, 0.85f, 0.45f}, {0.f, 0.f, 0.f}, 0.f, 0.8f, 0.95f, 1.1f, 0.02f, 0.6f, 0.f, 0.f, 0.5f, 1.f};
+
+     // pathtracer::object obj3{pathtracer::SPHERE,
+     //      pathtracer::sphere(pathtracer::mat4::get_translation(0.5f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
+     //      pathtracer::MICROFACET,
+     //      pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     // obj3.mat_d.microfacet = pathtracer::microfacet{{0.35f, 0.85f, 0.45f}, {0.f, 0.f, 0.f}, 0.f, 0.8f, 0.95f, 1.3f, 0.02f, 0.6f, 0.f, 0.f, 0.f, 1.f};
 
 
-        pathtracer::object obj0{pathtracer::SPHERE,
-             pathtracer::sphere(pathtracer::mat4::get_translation(-0.5f, 0.f, -0.8f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
-             pathtracer::LIGHT,
-             pathtracer::phong({0.95f, 0.25f, 0.5f}, 0.3, 0.7, 0.5, 10)};
-        obj0.mat_d.light = pathtracer::light({0.95f, 0.4f, 0.25});
+     // dim3 blocks(16, 16);
+     // dim3 threads(16, 16);
 
-        pathtracer::object obj1{pathtracer::SPHERE,
-             pathtracer::sphere(pathtracer::mat4::get_translation(-0.5f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
-             pathtracer::MICROFACET,
-             pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
-        obj1.mat_d.microfacet = pathtracer::microfacet{{0.35f, 0.25f, 0.85f}, {0.f, 0.f, 0.f}, 1.f, 0.18f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
+     // pathtracer::world world({
+     //      &obj0, &obj1, &obj2, &obj3
+     // }, {"teapot_full.obj"}, {pathtracer::mat4::get_scaling(0.01f, 0.01f, 0.01f)}, {{"cursed.exr", 618, 1100}}, "env.exr", blocks, threads);
 
-        pathtracer::object obj2{pathtracer::SPHERE,
-             pathtracer::sphere(pathtracer::mat4::get_translation(0.5f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
-             pathtracer::MICROFACET,
-             pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
-        obj2.mat_d.microfacet = pathtracer::microfacet{{0.35f, 0.85f, 0.45f}, {0.f, 0.f, 0.f}, 0.f, 0.8f, 0.95f, 1.1f, 0.02f, 0.6f, 0.f, 0.f, 0.5f, 1.f};
+     // std::string filename1 = "main_render_no_AS";
+     // std::string filename2 = "main_render_AS";
 
-        pathtracer::object obj3{pathtracer::SPHERE,
-             pathtracer::sphere(pathtracer::mat4::get_translation(0.5f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
-             pathtracer::MICROFACET,
-             pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
-        obj3.mat_d.microfacet = pathtracer::microfacet{{0.35f, 0.85f, 0.45f}, {0.f, 0.f, 0.f}, 0.f, 0.8f, 0.95f, 1.3f, 0.02f, 0.6f, 0.f, 0.f, 0.f, 1.f};
+     // pathtracer::render(camera, world, filename1, 100, false, 0.05f);
+     // pathtracer::render(camera, world, filename2, 1000, true, 0.05f);
+
+     // world.free_world();
+
+     pathtracer::object obj0{pathtracer::SPHERE,
+          pathtracer::sphere(pathtracer::mat4::get_translation(-0.5f, 0.f, -0.8f) * pathtracer::mat4::get_scaling(0.25f, 0.25f, 0.25f)),
+          pathtracer::LIGHT,
+          pathtracer::phong({0.95f, 0.25f, 0.5f}, 0.3, 0.7, 0.5, 10)};
+     obj0.mat_d.light = pathtracer::light({0.95f, 0.85f, 0.85f});
+
+     pathtracer::object obj1{pathtracer::SPHERE,
+          pathtracer::sphere(pathtracer::mat4::get_translation(-0.76f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
+          pathtracer::MICROFACET,
+          pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     obj1.mat_d.microfacet = pathtracer::microfacet{{0.95f, 0.25f, 0.05f}, {0.f, 0.f, 0.f}, 0.80f, 0.5f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
+
+     pathtracer::object obj2{pathtracer::SPHERE,
+          pathtracer::sphere(pathtracer::mat4::get_translation(-0.35f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
+          pathtracer::MICROFACET,
+          pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     obj2.mat_d.microfacet = pathtracer::microfacet{{0.95f, 0.25f, 0.05f}, {0.f, 0.f, 0.f}, 0.85f, 0.25f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
+
+     pathtracer::object obj3{pathtracer::SPHERE,
+          pathtracer::sphere(pathtracer::mat4::get_translation(0.f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
+          pathtracer::MICROFACET,
+          pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     obj3.mat_d.microfacet = pathtracer::microfacet{{0.95f, 0.25f, 0.05f}, {0.f, 0.f, 0.f}, 0.90f, 0.18f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
+
+     pathtracer::object obj4{pathtracer::SPHERE,
+          pathtracer::sphere(pathtracer::mat4::get_translation(0.35f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
+          pathtracer::MICROFACET,
+          pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     obj4.mat_d.microfacet = pathtracer::microfacet{{0.95f, 0.25f, 0.05f}, {0.f, 0.f, 0.f}, 0.95f, 0.14f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
+
+     pathtracer::object obj5{pathtracer::SPHERE,
+          pathtracer::sphere(pathtracer::mat4::get_translation(0.76f, 0.f, 0.f) * pathtracer::mat4::get_scaling(0.15f, 0.15f, 0.15f)),
+          pathtracer::MICROFACET,
+          pathtracer::phong{{0.f, 0.f, 0.f}, 0.f, 0.f, 0.f, 0.f}};
+     obj5.mat_d.microfacet = pathtracer::microfacet{{0.95f, 0.25f, 0.05f}, {0.f, 0.f, 0.f}, 0.99f, 0.1f, 0.f, 1.f, 0.04f, 0.f, 0.f, 0.25f, 0.f, 1.f};
 
 
-        dim3 blocks(16, 16);
-        dim3 threads(16, 16);
+     dim3 blocks(16, 16);
+     dim3 threads(16, 16);
 
-        pathtracer::world world({
-            &obj0, &obj1, &obj2, &obj3
-        }, {"teapot_full.obj"}, {pathtracer::mat4::get_scaling(0.01f, 0.01f, 0.01f)}, {{"cursed.exr", 618, 1100}}, "env.exr", blocks, threads);
+     pathtracer::world world({
+          &obj0, &obj1, &obj2, &obj3, &obj4, &obj5
+     }, {}, {}, {}, "mountains.exr", blocks, threads);
 
-        std::string filename1 = "main_render_no_AS";
-        std::string filename2 = "main_render_AS";
+     std::string filename1 = "materials_demo_no_AS";
+     std::string filename2 = "materials_demo_AS";
 
-        pathtracer::render(camera, world, filename1, 100, false, 0.05f);
-        pathtracer::render(camera, world, filename2, 200, true, 0.05f);
+     pathtracer::render(camera, world, filename1, 100, false, 0.05f);
+     // pathtracer::render(camera, world, filename2, 1000, true, 0.05f);
 
-        world.free_world();
+     world.free_world();
 
 }
